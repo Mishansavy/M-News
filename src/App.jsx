c33678a2720473e35aead75118872be3d9c5c4c8
Sudhan Shahi <ss.dev.n06@gmail.com>
2024-09-24 16:00:55 +0545

    fix pagination export code and routes 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Search from "./Components/Search.jsx";
-import Pagination from "./components/Pagination.jsx";
-import Stories from "./components/Stories.jsx";
+import Pagination from "./Components/pagination.jsx";
+import Stories from "./Components/Stories.jsx";
 import "./app.css";
 const App = () => {
   const websitename = "Mishan News";
