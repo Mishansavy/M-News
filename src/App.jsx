8de106e636f59eaa60c430952cde76d3d2d049ee
Sudhan Shahi <ss.dev.n06@gmail.com>
2024-09-24 15:42:47 +0545

    fix pagination export code and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Pagination from "./components/Pagination.jsx";
 import { Stories } from "./components/Stories.jsx";
+import Pagination from "./components/Pagination.jsx";
 import Search from "./components/Search.jsx";
 import "./app.css";
 const App = () => {
--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "../app.css";
 import { useGlobalContext } from "../context";
-export const pagination = () => {
+
+function Pagination() {
   const { Page, nbPages, getPrevPage, getNextpage } = useGlobalContext();
+
   return (
-    <>
-      <div className="pagination-area">
-        <button onClick={() => getPrevPage()}>previous</button>
-        <p>
-          {Page + 1} of {nbPages}
-        </p>
-        <button onClick={() => getNextpage()}>Next</button>
-      </div>
-    </>
+    <div className="pagination-area">
+      <button onClick={getPrevPage}>Previous</button>
+      <p>
+        {Page + 1} of {nbPages}
+      </p>
+      <button onClick={getNextpage}>Next</button>
+    </div>
   );
-};
-export default pagination;
+}
+
+export default Pagination;
